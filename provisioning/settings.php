766b7a3636389470865c443489d1c09f1b3a9cf7
Johannes Ekberg <uppfinnarn@gmail.com>
2014-10-23 10:48:25 +0200

    Complete provisioning; requires an SP running at 10.2.0.30

---
+++ provisioning/settings.php
@@ -0,0 +1,137 @@
+<?php
+
+/*
+ * This is the configuration file, in PHP format. In most cases, it's
+ * ok just to edit our settings, and remove the // comments.
+ */
+
+/*
+ * System "nodes" are the most important directives; they specify where
+ * your Halon mail gateway(s) are, and how to access it/them. It might be
+ * a good idea to read about authentication scripts on our wiki, to create
+ * specific access rights so that if this end-user web is compromised, your
+ * gateways are not. In some cases, even read-only access is good enough for
+ * this application.
+ */
+
+$settings['node'][] = array(
+		'address' => 'http://10.2.0.30/',
+		'username' => 'admin',
+		'password' => 'admin',
+		);
+//$settings['node'][] = array(
+//		'address' => 'https://10.2.0.31/',
+//		'username' => 'admin',
+//		'password' => 'admin',
+//		);
+
+/*
+ * The API key is used by the Halon mail gateways to communicate with
+ * this application, such as creating database users when messages are
+ * quarantined, or doing black/whitelist lookups.
+ */
+
+$settings['api-key'] = 'secret';
+
+/*
+ * The "mail from" and "public-url" settings are used by this application
+ * as self identification, for example in mail such as forgot password
+ * reminders or digest lists. The default source determine what's shown
+ * on the first page, or when pressing "messages" in the menu.
+ */
+
+//$settings['mail']['from'] = 'Mail quarantine <postmaster@example.org>';
+//$settings['public-url'] = 'http://10.2.0.166/enduser/';
+//$settings['default-source'] = 'history';
+//$settings['display-scores'] = false;
+//$settings['display-textlog'] = false;
+//$settings['display-listener']['mailserver:1'] = 'Inbound';
+//$settings['display-transport']['mailtransport:2'] = 'Internet';
+
+/*
+ * It's possible to use this application completely without a database.
+ * However, features such as local users (if SMTP or LDAP authentication
+ * is not suitable) and black/whitelisting requires a database. You can use
+ * most databases, such as SQLite, MySQL and PostgreSQL. Below are a few
+ * examples. You should use PHP PDO format.
+ */
+
+//$settings['database']['dsn'] = 'sqlite:/tmp/foo.db';
+//$settings['database']['dsn'] = 'pgsql:host=localhost;port=5432;dbname=spenduser;user=halon;password=halon';
+//$settings['database']['dsn'] = 'mysql:host=localhost;port=5432;dbname=spenduser';
+//$settings['database']['user'] = 'root';
+//$settings['database']['password'] = '1';
+$settings['database']['dsn'] = 'sqlite:/vagrant/enduser.db';
+
+/*
+ * Authentication is probably the second most important configuration
+ * directive, as it specifies how end-users should identify themselves.
+ * You can use the following types:
+ *  - LDAP, against for example an Exchange server
+ *  - SMTP (SASL), against a mail server, if the username is an e-mail
+ *  - Database, populated by the Halon mail gateways when mail are quarantined
+ *  - Local accounts, statically configured in this file (with access rights).
+ *    Use lower case letters when manually adding an access level.
+ */
+
+//$settings['authentication'][] = array(
+//		'type' => 'database',
+//		);
+$settings['authentication'][] = array(
+		'type' => 'account',
+		'username' => 'foo',
+		'password' => 'foo',
+		);
+//$settings['authentication'][] = array(
+//		'type' => 'ldap',
+//		'uri' => 'ldap://10.2.7.2',
+//		'base_dn' => 'CN=Users,DC=dev,DC=halon,DC=local',
+//		'schema' => 'msexchange',
+//		);
+//$settings['authentication'][] = array(
+//		'type' => 'smtp',
+//		'host' => '10.2.0.30',
+//		'port' => 25,
+//		);
+
+/*
+ * The quarantine filter is used to restrict the end-user access to
+ * only certain quarantines, in case you have multiple quarantines with
+ * different purposes.
+ */
+
+//$settings['quarantine-filter'][] = 'mailquarantine:1';
+//$settings['quarantine-filter'][] = 'mailquarantine:2';
+
+/*
+ * The default filter-pattern to use when creating additional
+ * inbound/outbound restrictions are "{from} or {to}", however
+ * in some cases it's necessary to know if the message is
+ * inbound or outbound.
+ */
+
+//$settings['filter-pattern'] = '{from} server=mailserver:2 or {to} server=mailserver:1';
+
+/*
+ * It's possible to send "digest" messages with a list of what's in
+ * the quarantine. It is added as a cron job, to be run every 24 hours:
+ * # php cron.php.txt digestday
+ * and it will use the authentication sources to find users. To use LDAP,
+ * add a 'bind_dn' and 'bind_password' to your LDAP source. To use static
+ * users (type account), add a 'email' to them. To send digest messages to
+ * EVERY RECIPIENT (user or not) that has quarantine messages, enable the
+ * to-all option below. To have a "direct release link" in the messages,
+ * enable the digest secret below.
+ */
+
+//$settings['digest']['to-all'] = true;
+//$settings['digest']['secret'] = 'badsecret';
+
+/*
+ * If hosting multiple websites on the same server, it's important to use
+ * different session names for each site.
+ */
+
+//$settings['session-name'] = 'spenduser';
+
+?>
