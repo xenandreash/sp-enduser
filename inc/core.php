13d618a0f44836d14cc89a07c801c0f91713bc77
Johannes Ekberg <uppfinnarn@gmail.com>
2014-10-23 11:18:14 +0200

    New way to assign defaults

--- inc/core.php
+++ inc/core.php
@@ -9,17 +9,27 @@ error_reporting(E_ALL ^ E_NOTICE);
 
 require_once 'utils.php';
 
+function set_default(&$settings, $key, $value) {
+	// Assign a default value if the value is absent, or if it's an empty array
+	if(!isset($settings[$key]) || (is_array($settings[$key]) && empty($settings[$key])))
+		$settings[$key] = $value;
+}
+
 function settings() {
 	$base = dirname($_SERVER['SCRIPT_FILENAME']);
 	$settings = array();
-	// default values
-	$settings['public-url'] = self_url();
+	
 	if (!file_exists($base.'/settings.php'))
 		die('Missing '.$base.'/settings.php; edit settings.php.default and rename it');
 	require $base.'/settings.php';
+	
+	// default values
+	set_default($settings, 'public-url', self_url());
+	
 	$tmp = $settings;
 	foreach (func_get_args() as $arg)
 		$tmp = $tmp[$arg];
+	
 	return $tmp;
 }
 
