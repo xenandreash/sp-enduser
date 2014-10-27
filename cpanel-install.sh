0ecd8e6f47a19264ce52afa57274de749aa9d7f5
Johannes Ekberg <uppfinnarn@gmail.com>
2014-10-27 12:09:48 +0100

    Fancy installer + uninstaller scripts

--- cpanel-install.sh
+++ cpanel-install.sh
@@ -1,18 +1,115 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ ! -x '/usr/local/cpanel/bin/register_cpanelplugin' ];
-then
-	echo 'No register_cpanelplugin'
+# Exit if anything errors; better safe than sorry
+set -e
+
+if [ $EUID -ne 0 ]; then
+	echo "This script requires root privileges, please run it as root, or with:"
+	echo "    sudo ./cpanel-install.sh"
 	exit 1
 fi
 
-rm -rf /usr/local/cpanel/base/3rdparty/sp-enduser
-cp -R . /usr/local/cpanel/base/3rdparty/sp-enduser
-rm -f /usr/local/cpanel/base/3rdparty/sp-enduser/install.php
-mv /usr/local/cpanel/base/3rdparty/sp-enduser/settings-default.php \
-	/usr/local/cpanel/base/3rdparty/sp-enduser/settings.php
+if [ ! -x '/usr/local/cpanel/bin/register_cpanelplugin' ];
+then
+	echo "Can't seem to find register_cpanelplugin, is cPanel installed properly?"
+	exit 1
+fi
 
-/usr/local/cpanel/bin/register_cpanelplugin cpanel-sp-enduser.cpanelplugin
-echo 'Halon SP-enduser interface installed into cPanel.'
-echo 'Configure: /usr/local/cpanel/base/3rdparty/sp-enduser/settings.php'
-exit
+
+
+# Figure out where we are; relative paths are evil
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Destination path, because nobody likes repeating themselves
+DEST="/usr/local/cpanel/base/3rdparty/sp-enduser"
+
+
+
+echo "Installing SP-Enduser's cPanel plugin to:"
+echo "$DEST"
+echo ""
+
+# Only move things if they're not already in the right place
+if [[ "$DIR" != "$DEST" ]]; then
+	
+	# If there's an old (copied) installation, abort and tell the user
+	if [[ -L $DEST ]]; then
+		echo "An existing link has been found, updating the link..."
+		rm $DEST
+		ln -s $DIR $DEST
+	elif [[ -d $DEST ]]; then
+		echo "An existing installation has been found."
+		echo ""
+		echo "Please either run 'git pull' and './cpanel-install.sh' in the existing"
+		echo "installation to update it, or move it aside to set up a new installation."
+		exit 1
+	else
+		echo "There are two ways to install SP-Enduser, choose the one you would like:"
+		echo ""
+		echo "== Copy this directory to the destination =="
+		echo ""
+		echo "   This is the easiest way to get SP-Enduser installed as a cPanel plugin,"
+		echo "   but you can't, for instance, share a single installation between cPanel"
+		echo "   and Apache, if you're going for that kind of setup."
+		echo ""
+		echo "   With this kind of install, the source directory is no longer needed."
+		echo ""
+		echo "== Link the source to the destination =="
+		echo ""
+		echo "   This will allow you to keep your SP-Enduser installation somewhere other"
+		echo "   than in cPanel's files, for instance if you want to share a single"
+		echo "   installation between cPanel and Apache, or you just want easier access"
+		echo "   for configuration and custom modifications."
+		echo ""
+		echo "   With this kind of install, this directory needs to remain where it is!"
+		echo "   If you move it, you must update the link, or re-run this script and"
+		echo "   it'll do it for you."
+		echo ""
+		
+		keep_around=false
+		select type in "Copy" "Link"; do
+			case $type in
+				"Copy")
+					echo ""
+					echo "Copy selected, installing..."
+					cp -Rf "$DIR" "$DEST"
+					break;;
+				"Link")
+					echo ""
+					echo "Link installed, installing..."
+					ln -s "$DIR" "$DEST"
+					keep_around=true
+					break;;
+			esac
+		done
+		
+		# Register the plugin with cPanel; output is written 
+		echo "Registering the plugin with cPanel... (this may take a while)"
+		if /usr/local/cpanel/bin/register_cpanelplugin $DIR/cpanel-sp-enduser.cpanelplugin > $DIR/register_cpanelplugin.log; then
+			# If the installation succeeds, just quietly delete the log file
+			rm $DIR/register_cpanelplugin.log
+		else
+			echo " -> Couldn't register the plugin!"
+			echo "    Output has been written to register_cpanelplugin.log"
+			exit 1
+		fi
+		
+		echo ""
+		echo "Halon SP-Enduser has been successfully installed!"
+		echo ""
+		
+		if $keep_around; then
+			echo "Remember to keep this directory around - the installation is a link, and will"
+			echo "cease to function if this is moved or deleted."
+			echo ""
+			echo "If you want to move or rename it, remember to re-run this script to relink it."
+		else
+			echo "You can safely delete this directory, it has been copied to:"
+			echo "$DEST"
+		fi
+		
+		echo ""
+		echo "To configure SP-Enduser, copy settings-default.php to settings.php and edit it."
+		echo "Check the Wiki for installation instructions: http://wiki.halon.se/End-user"
+	fi
+fi
--- cpanel-uninstall.sh
+++ cpanel-uninstall.sh
@@ -1,10 +1,66 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ ! -x '/usr/local/cpanel/bin/unregister_cpanelplugin' ];
-then
-	echo 'No unregister_cpanelplugin'
+# 
+# Most comments are in cpanel-install.sh
+# 
+
+set -e
+
+if [ $EUID -ne 0 ]; then
+	echo "This script requires root privileges, please run it as root, or with:"
+	echo "    sudo ./cpanel-uninstall.sh"
 	exit 1
 fi
 
-rm -rf /usr/local/cpanel/base/3rdparty/sp-enduser
-/usr/local/cpanel/bin/unregister_cpanelplugin cpanel-sp-enduser.cpanelplugin
+if [ ! -x '/usr/local/cpanel/bin/unregister_cpanelplugin' ];
+then
+	echo "Can't seem to find unregister_cpanelplugin, is cPanel installed properly?"
+	exit 1
+fi
+
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DEST="/usr/local/cpanel/base/3rdparty/sp-enduser"
+
+if [[ ! -d $DEST ]]; then
+	echo "The SP-Enduser cPanel plugin is not installed!"
+	exit 1
+fi
+
+
+
+echo "Unregistering the plugin with cPanel..."
+if /usr/local/cpanel/bin/unregister_cpanelplugin $DIR/cpanel-sp-enduser.cpanelplugin > $DIR/unregister_cpanelplugin.log; then
+	rm $DIR/unregister_cpanelplugin.log
+else
+	echo " -> Couldn't unregister the plugin!"
+	echo "    Output has been written to unregister_cpanelplugin.log"
+	exit 1
+fi
+echo ""
+
+echo "Plugin unregistered, what would you like to do with your SP-Enduser install?"
+if [[ -L $DEST ]]; then
+	echo ""
+	echo "Your installation is linked, so choosing 'Delete it' will only delete the link."
+	echo "To permanently get rid of it, manually delete the source folder:"
+	echo "    `readlink -f $DEST`"
+else
+	echo "Warning: Deletion is final, and can't be undone!"
+fi
+
+echo ""
+select action in "Leave it" "Delete it"; do
+	case $action in
+		"Leave it")
+			echo "Your installation has been left intact."
+			echo "You can find it at:"
+			echo "    $DEST"
+			break;;
+		"Delete it")
+			rm -rf $DEST
+			echo "SP-Enduser has been deleted."
+			break;;
+	esac
+done
