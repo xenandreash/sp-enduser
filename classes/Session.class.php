c39e6bb562b324476ae6ddc92a91f0c7917974a6
Johannes Ekberg <johannes@halon.se>
2014-10-30 09:00:46 +0100

    Documentation is nice too

--- classes/Session.class.php
+++ classes/Session.class.php
@@ -1,12 +1,22 @@
 <?php
 require_once BASE.'/inc/core.php';
 
+/**
+ * Singleton wrapper for the user's session.
+ * 
+ * Note that the cPanel LivePHP version uses a different implementation of
+ * this class, defined in cpanel-sp-enduser.live.php, that uses the cPanel
+ * user's session instead.
+ */
 class Session
 {
 	private $username = null;
 	private $source = null;
 	private $access = null;
 	
+	/**
+	 * Returns a shared Session instance.
+	 */
 	public static function Get()
 	{
 		static $inst = null;
@@ -15,6 +25,9 @@ class Session
 		return $inst;
 	}
 	
+	/**
+	 * Private constructor; use Session::Get().
+	 */
 	private function __construct()
 	{
 		$session_name = settings('session-name');
@@ -28,16 +41,45 @@ class Session
 		$this->access = $_SESSION['access'];
 	}
 	
+	/**
+	 * Returns the user's username.
+	 * 
+	 * What exactly this means is a bit dependent on the configured
+	 * authentication methods - it may be basically anything, but should be
+	 * written out as-is.
+	 */
 	public function getUsername()
 	{
 		return $this->username;
 	}
 	
+	/**
+	 * Returns the user's authentication source.
+	 * 
+	 * This can be one of:
+	 * 
+	 *   - account:  A local account defined in settings.php
+	 *   - smtp:     Login accepted by an SMTP server
+	 *   - ldap:     Login accepted by an LDAP server
+	 *   - database: An account defined in an SQL database
+	 *   - server:   Authenticated directly against an SP node
+	 */
 	public function getSource()
 	{
 		return $this->source;
 	}
 	
+	/**
+	 * Returns the user's access parameters (permissions).
+	 * 
+	 * This is an array with several keys that, if given, restrict the user to
+	 * only the given realms. An empty array means no access.
+	 * 
+	 * Possible keys:
+	 * 
+	 *   - mail: Can only see records involving the given email address(es)
+	 *   - domain: Can only see records involving the given domain(s)
+	 */
 	public function getAccess()
 	{
 		return $this->access;
