cd6a08285eb71610dfb364f89c2367961187afd6
Johannes Ekberg <johannes@halon.se>
2014-10-30 11:28:50 +0100

    Deleted old settings code

--- inc/core.php
+++ inc/core.php
@@ -14,32 +14,6 @@ function halon_autoloader($class) {
 }
 spl_autoload_register('halon_autoloader');
 
-function set_default(&$settings, $key, $value) {
-	// Assign a default value if the value is absent, or if it's an empty array
-	if(!isset($settings[$key]) || (is_array($settings[$key]) && empty($settings[$key])))
-		$settings[$key] = $value;
-}
-
-/*function settings() {
-	$settings = array();
-	
-	if (!file_exists(BASE.'/settings.php'))
-		die('Missing '.BASE.'/settings.php; edit settings-default.php and rename it');
-	require BASE.'/settings.php';
-	
-	// default values
-	set_default($settings, 'public-url', self_url());
-	set_default($settings, 'authentication', array( array( 'type' => 'server' ) ));
-	
-	$tmp = $settings;
-	foreach (func_get_args() as $arg)
-		$tmp = $tmp[$arg];
-	
-	return $tmp;
-}
-
-$settings = settings();*/
-
 $settings = Settings::Get();
 
 // Always use UTC timezone
