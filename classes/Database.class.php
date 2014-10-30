7698c0ee0c2fd0633904d6e1fdd8bcc26d9935ab
Johannes Ekberg <johannes@halon.se>
2014-10-30 11:37:37 +0100

    Eliminate the need for dsn checks everywhere

--- classes/Database.class.php
+++ classes/Database.class.php
@@ -8,7 +8,13 @@ class Database extends PDO
 	public function __construct($credentials = null)
 	{
 		if($credentials === null)
+		{
 			$credentials = Settings::Get()->getDBCredentials();
+			
+			// This is ugly, but better than repeatedly repeating ourselves
+			if(!$credentials['dsn'])
+				die("No database configured");
+		}
 		
 		$dsn = $credentials['dsn'];
 		$username = isset($credentials['user']) ? $credentials['user'] : null;
