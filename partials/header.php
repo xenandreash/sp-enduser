532d6b013584e39ae22fc882ada974f5d01bdd03
Johannes Ekberg <johannes@halon.se>
2014-10-30 15:29:47 +0100

    And a relative import fixed

--- partials/header.php
+++ partials/header.php
@@ -2,7 +2,7 @@
 
 // XXX this file is included from login.php as well
 
-require_once 'core.php';
+require_once BASE.'/inc/core.php';
 
 $pagename = $settings->getPageName();
 $title = $title ?: 'Untitled';
