08b87cc16e7bcdcbf3c5bf2b2d8684fb9c5484e9
Johannes Ekberg <johannes@halon.se>
2015-01-08 13:56:09 +0100

    Re-layouted preview with panels and columns

--- pages/preview.php
+++ pages/preview.php
@@ -104,107 +104,133 @@ require_once BASE.'/partials/header.php';
 				<?php } ?>
 			</form>
 		</div>-->
-		<dl class="dl-horizontal">
-			<?php if ($listener) { ?>
-				<dt>Received by</dt>
-				<dd><?php echo $listener ?></dd>
-			<?php } ?>
+		<!-- <div class="row"> -->
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Metadata</h3>
+					</div>
+					<div class="panel-body">
+						<dl class="dl-horizontal">
+							<?php if ($listener) { ?>
+								<dt>Received by</dt>
+								<dd><?php echo $listener ?></dd>
+							<?php } ?>
+							
+							<dt>Date</dt>
+							<dd><?php p(strftime('%Y-%m-%d %H:%M:%S', $mail->msgts0 - $_SESSION['timezone'] * 60)) ?></dd>
+							
+							<dt>Server</dt><dd><?php p($mail->msgfromserver) ?></dd>
+							<?php if ($mail->msgsasl) { ?><dt>User</dt><dd><?php p($mail->msgsasl) ?></dd><?php } ?>
+							
+							<dt>From</dt>
+							<dd><?php p($mail->msgfrom) ?></dd>
+							
+							<dt>To</dt>
+							<dd><?php p($mail->msgto) ?></dd>
+							
+							<dt>Subject</dt>
+							<dd><?php p($mail->msgsubject) ?></dd>
+							
+							<dt>Action</dt>
+							<dd><?php p(ucfirst(strtolower($mail->msgaction))) ?></dd>
+							
+							<?php if ($desc) { ?>
+							<dt>Details</dt>
+							<dd><?php echo $desc ?></dd>
+							<?php } ?>
+							
+							<?php if ($transport) { ?>
+							<dt>Destination</dt>
+							<dd><?php echo $transport ?></dd>
+							<?php } ?>
+							
+							<dt>ID</dt>
+							<dd><?php p($mail->msgid) ?></dd>
+						</dl>
+					</div>
+				</div>
+				
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Scores</h3>
+					</div>
+					<?php if ($scores) { ?>
+						<table class="table">
+							<thead>
+								<tr>
+									<th>Engine</th>
+									<th>Result</th>
+									<th class="hidden-xs">Signature</th>
+								</tr>
+							</thead>
+							<tbody>
+							<?php
+							$scores = history_parse_scores($mail);
+							foreach ($scores as $score) { ?>
+								<tr>
+								<td><?php p($score['name']) ?></td>
+								<td><?php p($score['score']) ?></td>
+								<td class="semitrans hidden-xs"><?php p($score['text']) ?></td>
+								</tr>
+							<?php } ?>
+							</tbody>
+						</table>
+					<?php } ?>
+				</div>
+			</div>
 			
-			<dt>Date</dt>
-			<dd><?php p(strftime('%Y-%m-%d %H:%M:%S', $mail->msgts0 - $_SESSION['timezone'] * 60)) ?></dd>
-			
-			<dt>Server</dt><dd><?php p($mail->msgfromserver) ?></dd>
-			<?php if ($mail->msgsasl) { ?><dt>User</dt><dd><?php p($mail->msgsasl) ?></dd><?php } ?>
-			
-			<dt>From</dt>
-			<dd><?php p($mail->msgfrom) ?></dd>
-			
-			<dt>To</dt>
-			<dd><?php p($mail->msgto) ?></dd>
-			
-			<dt>Subject</dt>
-			<dd><?php p($mail->msgsubject) ?></dd>
-			
-			<dt>Action</dt>
-			<dd><?php p(ucfirst(strtolower($mail->msgaction))) ?></dd>
-			
-			<?php if ($desc) { ?>
-			<dt>Details</dt>
-			<dd><?php echo $desc ?></dd>
-			<?php } ?>
-			
-			<?php if ($transport) { ?>
-			<dt>Destination</dt>
-			<dd><?php echo $transport ?></dd>
-			<?php } ?>
-			
-			<dt>ID</dt>
-			<dd><?php p($mail->msgid) ?></dd>
-		</dl>
-		
-		<hr />
-		
-		<div class="msg-body">
-			<?php
-			if ($encode == 'TEXT')
-				echo '<pre>'.$body.'</pre>';
-			else if ($body)
-				echo $body;
-			?>
+			<div class="col-md-8">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Content</h3>
+					</div>
+					<div class="panel-body">
+						<div class="msg-body">
+							<?php
+							if ($encode == 'TEXT')
+								echo '<pre>'.$body.'</pre>';
+							else if ($body)
+								echo $body;
+							?>
+						</div>
+						
+						<?php if (count($attachments) > 0) { ?>
+							<hr />
+							
+							<div class="preview-attachments">
+							<?php foreach ($attachments as $a) { ?>
+								<div class="preview-attachment"><?php p($a[2]) ?> (<?php echo round($a[1]/1024, 0) ?> KiB)</div>
+							<?php } ?>
+							</div>
+						<?php } ?>
+					</div>
+				</div>
+				
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Headers</h3>
+					</div>
+					<div class="panel-body">
+						<?php if ($header != '') { ?>
+							<div class="msg-header" style="clear:both;margin-top:5px;">
+								<div style="float:left;margin:5px;height:8px;width:8px;background-color:#ddffdd;border: 1px solid #ccc;"></div>
+								<div style="float:left;font-size:10px;padding-top:5px;color:green;margin-right:10px;">Added</div>
+								<div style="float:left;margin:5px;height:8px;width:8px;background-color:#ffdddd;border: 1px solid #ccc;"></div>
+								<div style="float:left;font-size:10px;padding-top:5px;color:red;">Removed</div>
+								<div class="preview-headers"></div>
+							</div>
+							<script>
+								var headers_original = <?php echo json_encode($header); ?>;
+								var headers_modified = <?php echo json_encode($headerdelta); ?>;
+								$(".preview-headers").html(diff_lineMode(headers_original,
+									headers_modified ? headers_modified : headers_original, true));
+							</script>
+						<?php } ?>
+					</div>
+				</div>
+			</div>
 		</div>
-		
-		<hr />
-		
-		<?php if (count($attachments) > 0) { ?>
-			<div class="preview-attachments">
-			<?php foreach ($attachments as $a) { ?>
-				<div class="preview-attachment"><?php p($a[2]) ?> (<?php echo round($a[1]/1024, 0) ?> KiB)</div>
-			<?php } ?>
-			</div>
-			
-			<hr />
-		<?php } ?>
-		
-		<?php if ($header != '') { ?>
-			<div class="msg-header" style="clear:both;margin-top:5px;">
-				<div style="float:left;margin:5px;height:8px;width:8px;background-color:#ddffdd;border: 1px solid #ccc;"></div>
-				<div style="float:left;font-size:10px;padding-top:5px;color:green;margin-right:10px;">Added</div>
-				<div style="float:left;margin:5px;height:8px;width:8px;background-color:#ffdddd;border: 1px solid #ccc;"></div>
-				<div style="float:left;font-size:10px;padding-top:5px;color:red;">Removed</div>
-				<div class="preview-headers"></div>
-			</div>
-			<script>
-				var headers_original = <?php echo json_encode($header); ?>;
-				var headers_modified = <?php echo json_encode($headerdelta); ?>;
-				$(".preview-headers").html(diff_lineMode(headers_original,
-					headers_modified ? headers_modified : headers_original, true));
-			</script>
-			
-			<hr />
-		<?php } ?>
-		
-		<?php if ($scores) { ?>
-			<table class="table">
-				<thead>
-					<tr>
-						<th>Engine</th>
-						<th>Result</th>
-						<th class="hidden-xs">Signature</th>
-					</tr>
-				</thead>
-				<tbody>
-				<?php
-				$scores = history_parse_scores($mail);
-				foreach ($scores as $score) { ?>
-					<tr>
-					<td><?php p($score['name']) ?></td>
-					<td><?php p($score['score']) ?></td>
-					<td class="semitrans hidden-xs"><?php p($score['text']) ?></td>
-					</tr>
-				<?php } ?>
-				</tbody>
-			</table>
-		<?php } ?>
 		
 		<form id="actionform" method="post" action="?page=preview&node=<?php p($node) ?>&id=<?php p($id) ?>">
 			<input type="hidden" name="action" id="action" value="">
