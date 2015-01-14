349f9944b95a7156d4057be845fb04a7e22b9582
Johannes Ekberg <johannes@halon.se>
2015-01-14 16:56:43 +0100

    Disable user scaling

--- partials/header.php
+++ partials/header.php
@@ -22,7 +22,7 @@ $dbCredentials = $settings->getDBCredentials();
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 		<title><?php echo $title ?> | <?php echo $pagename ?></title>
 		<link rel="stylesheet" href="static/css/bootstrap.min.css">
 		<link rel="stylesheet" href="<?php echo $styles ?>">
