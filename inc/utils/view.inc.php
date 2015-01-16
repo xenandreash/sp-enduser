3a0312bf9e5d8c4604a5bfc54c0f7837a9d2b833
Johannes Ekberg <johannes@halon.se>
2015-01-16 16:53:43 +0100

    Docs and chaining

--- inc/utils/view.inc.php
+++ inc/utils/view.inc.php
@@ -1,7 +1,16 @@
 <?php
 
+/**
+ * Safely prints a value.
+ * 
+ * The second parameter is a default, which is assumed to be trusted, and not
+ * escaped. To fall back to an untrusted value, use something like:
+ * 
+ *     <?php p($var) or p($default) ?>
+ */
 function p($str, $def = "") {
 	echo $str ? htmlspecialchars($str) : $def;
+	return (bool)$str;
 }
 
 function p_select($name, $selected, $options, $extra = "") {
