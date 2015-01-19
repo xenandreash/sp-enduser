0d28c62ed30ce8f736a8c3595ed8d10b54298b10
Johannes Ekberg <johannes@halon.se>
2015-01-19 13:50:40 +0100

    Handle spam filter messages

--- inc/utils/view.inc.php
+++ inc/utils/view.inc.php
@@ -25,15 +25,22 @@ function p_select($name, $selected, $options, $extra = "") {
 }
 
 /**
- * Parses an SMTP status string, returning an array of the status message and
- * the extended description.
+ * Parses a status string, returning an array of the status message and
+ * the extended description. Can handle either SMTP status lines or freeform
+ * strings where the first word is the action.
  * 
  * @param $part If given, the index of the array to return alone (0 or 1)
  */
-function smtp_status_parse($status, $part = NULL) {
+function parse_status($status, $part = NULL) {
 	$parts0 = explode(' ', $status, 2);
 	$parts1 = explode(':', $parts0[1], 2);
-	$parts = array($parts1[0], ucfirst(trim($parts1[1])));
+	
+	$parts = array();
+	if (count($parts1) != 2)
+		$parts = array($parts0[0], $status);
+	else
+		$parts = array($parts1[0], ucfirst(trim($parts1[1])));
+	
 	return $part !== NULL ? $parts[$part] : $parts;
 }
 
@@ -47,7 +54,7 @@ function short_msg_status($m) {
 	else if ($m['type'] == 'queue' && $m['data']->msgaction == 'DELIVER')
 		return "In queue (retry ".$m['data']->msgretries.")";
 	else
-		return smtp_status_parse($m['data']->msgdescription, 0);
+		return parse_status($m['data']->msgdescription, 0);
 }
 
 /**
@@ -58,5 +65,5 @@ function long_msg_status($m) {
 	if ($m['type'] == 'queue' && $m['data']->msgaction == 'DELIVER')
 		return "Retry ".$m['data']->msgretries;
 	else
-		return smtp_status_parse($m['data']->msgdescription, 1) ?: $m['data']->msgdescription;
+		return parse_status($m['data']->msgdescription, 1) ?: $m['data']->msgdescription;
 }
