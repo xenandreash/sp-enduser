xenandreash/sp-enduser

commit 51c4762e15e39bf54c73bab4e915fc9de34e0819

design tweaks

pages/preview.php
@@ -72,6 +72,26 @@ if (isset($_POST['action'])) {
 	die();
 }
 
+$action_classes = array(
+	'DELIVER' => 'success',
+	'QUEUE' => 'info',
+	'QUARANTINE' => 'warning',
+	'REJECT' => 'danger',
+	'DELETE' => 'danger',
+	'ERROR' => 'warning',
+	'DEFER' => 'warning',
+);
+$action_icons = array(
+	'DELIVER' => 'ok',
+	'QUEUE' => 'transfer',
+	'QUARANTINE' => 'inbox',
+	'REJECT' => 'ban-circle',
+	'DELETE' => 'trash',
+	'ERROR' => 'exclamation-sign',
+	'DEFER' => 'warning-sign',
+);
+if ($type == 'queue' && $mail->msgaction == 'DELIVER') $mail->msgaction = 'QUEUE';
+
 // Prepare data
 $display_scores = $settings->getDisplayScores();
 $logs = $settings->getDisplayTextlog();
@@ -79,7 +99,7 @@ $transports = $settings->getDisplayTransport();
 $listeners = $settings->getDisplayListener();
 if (isset($transports[$mail->msgtransport])) $transport = $transports[$mail->msgtransport];
 if (isset($listeners[$mail->msglistener])) $listener = $listeners[$mail->msglistener];
-if ($type == 'queue' && $mail->msgaction == 'DELIVER')
+if ($mail->msgaction == 'QUEUE')
 	$desc = 'In queue (retry '.$mail->msgretries.')<br /><span class="text-muted">'.htmlspecialchars($mail->msgerror).'</span>';
 else
 	$desc = htmlspecialchars($mail->msgdescription);
@@ -137,7 +157,7 @@ require_once BASE.'/partials/header.php';
 					<li class="divider"></li>
 					<li><a data-action="delete"><i class="glyphicon glyphicon-trash"></i>&nbsp;Delete</a></li>
 					<li><a data-action="bounce"><i class="glyphicon glyphicon-repeat"></i>&nbsp;Bounce</a></li>
-					<li><a data-action="retry"><i class="glyphicon glyphicon-play"></i>&nbsp;Retry/release</a></li>
+					<li><a data-action="retry"><i class="glyphicon glyphicon-play-circle"></i>&nbsp;Retry/release</a></li>
 				<?php } ?>
 			</ul>
 		</div>
@@ -157,7 +177,7 @@ require_once BASE.'/partials/header.php';
 							<li class="divider"></li>
 							<li><a data-action="delete"><i class="glyphicon glyphicon-trash"></i>&nbsp;Delete message</a></li>
 							<li><a data-action="bounce"><i class="glyphicon glyphicon-repeat"></i>&nbsp;Bounce message</a></li>
-							<li><a data-action="retry"><i class="glyphicon glyphicon-play"></i>&nbsp;Retry/release message</a></li>
+							<li><a data-action="retry"><i class="glyphicon glyphicon-play-cicle"></i>&nbsp;Retry/release message</a></li>
 						<?php } ?>
 					</ul>
 				</li>
@@ -168,16 +188,19 @@ require_once BASE.'/partials/header.php';
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-5 col-md-push-7">
-				<div class="panel panel-default">
+				<div class="panel panel-default panel-<?php p($action_classes[$mail->msgaction]); ?>">
 					<div class="panel-heading">
 						<h3 class="panel-title">Details</h3>
 					</div>
 					<div class="panel-body">
 						<dl class="dl-horizontal">
+							<dt>Action</dt><dd>
+								<span class="glyphicon glyphicon-<?php echo $action_icons[$mail->msgaction] ?>"></span>
+								<?php p($mail->msgaction) ?>
+							</dd>
 							<?php if ($mail->msgfrom !== '') { ?><dt>From</dt><dd class="wrap"><?php p($mail->msgfrom) ?></dd><?php } ?>
 							<dt>To</dt><dd class="wrap"><?php p($mail->msgto) ?></dd>
 							<dt>Date</dt><dd><?php p(strftime('%Y-%m-%d %H:%M:%S', $mail->msgts0 - $_SESSION['timezone'] * 60)) ?></dd>
-							<dt>Action</dt><dd><?php p(ucfirst(strtolower($mail->msgaction))) ?></dd>
 							<?php if ($desc) { ?><dt>Details</dt><dd><?php pp($desc) ?></dd><?php } ?>
 							<?php if ($listener) { ?><dt>Received by</dt><dd><?php p($listener) ?></dd><?php } ?>
 							<dt>Server</dt><dd><?php p($mail->msgfromserver) ?></dd>

pages/index.php
@@ -40,10 +40,19 @@ $action_classes = array(
 	'DELIVER' => 'default',
 	'QUEUE' => 'info',
 	'QUARANTINE' => 'warning',
-	'BOUNCE' => 'danger',
 	'REJECT' => 'danger',
-	'ERROR' => 'danger',
-	'DEFER' => 'warning'
+	'DELETE' => 'danger',
+	'ERROR' => 'warning',
+	'DEFER' => 'warning',
+);
+$action_icons = array(
+	'DELIVER' => 'ok',
+	'QUEUE' => 'transfer',
+	'QUARANTINE' => 'inbox',
+	'REJECT' => 'ban-circle',
+	'ERROR' => 'exclamation-sign',
+	'DEFER' => 'warning-sign',
+	'DELETE' => 'trash',
 );
 
 function get_preview_link($m) {
@@ -189,7 +198,7 @@ $has_multiple_sources = count($sources) > 1;
 						<ul class="dropdown-menu" role="menu">
 							<li><a data-bulk-action="delete"><i class="glyphicon glyphicon-trash"></i>&nbsp;Delete selected</a></li>
 							<li><a data-bulk-action="bounce"><i class="glyphicon glyphicon-repeat"></i>&nbsp;Bounce selected</a></li>
-							<li><a data-bulk-action="retry"><i class="glyphicon glyphicon-play"></i>&nbsp;Retry/release selected</a></li>
+							<li><a data-bulk-action="retry"><i class="glyphicon glyphicon-play-circle"></i>&nbsp;Retry/release selected</a></li>
 						</ul>
 					</li>
 				</ul>
@@ -206,7 +215,7 @@ $has_multiple_sources = count($sources) > 1;
 					<ul class="dropdown-menu" role="menu">
 						<li><a data-bulk-action="delete"><i class="glyphicon glyphicon-trash"></i>&nbsp;Delete selected</a></li>
 						<li><a data-bulk-action="bounce"><i class="glyphicon glyphicon-repeat"></i>&nbsp;Bounce selected</a></li>
-						<li><a data-bulk-action="retry"><i class="glyphicon glyphicon-play"></i>&nbsp;Retry/release selected</a></li>
+						<li><a data-bulk-action="retry"><i class="glyphicon glyphicon-play-circle"></i>&nbsp;Retry/release selected</a></li>
 					</ul>
 				</li>
 			</ul>
@@ -235,9 +244,7 @@ $has_multiple_sources = count($sources) > 1;
 			<table class="table table-hover hidden-xs">
 				<thead>
 					<tr>
-						<?php if ($source != 'history') { ?>
-							<th style="width:30px">&nbsp;</th>
-						<?php } ?>
+						<th style="width:30px">&nbsp;</th>
 						<th class="hidden-xs">From</th>
 						<?php if ($has_multiple_addresses) { ?>
 						<th class="hidden-xs">To</th>
@@ -270,13 +277,16 @@ $has_multiple_sources = count($sources) > 1;
 						$i++;
 						$param[$m['type']][$m['id']]['offset']++;
 						$preview = get_preview_link($m);
+						if ($m['type'] == 'queue' && $m['data']->msgaction == 'DELIVER') $m['data']->msgaction = 'QUEUE';
 					?>
 					<tr class="<?php p($action_classes[$m['data']->msgaction]); ?>">
+						<td>
 						<?php if ($source != 'history') { ?>
-							<td>
-								<input type="checkbox" name="multiselect-<?php p($m['data']->id); ?>" value="<?php p($m['id']); ?>">
-							</td>
+							<input type="checkbox" name="multiselect-<?php p($m['data']->id); ?>" value="<?php p($m['id']); ?>">
+						<?php } else { ?>
+							<span class="glyphicon glyphicon-<?php echo $action_icons[$m['data']->msgaction] ?>"></span>
 						<?php } ?>
+						</td>
 						<td class="hidden-xs" data-href="<?php p($preview); ?>"><?php p($m['data']->msgfrom) ?></td>
 						<?php if ($has_multiple_addresses) { ?>
 						<td class="hidden-xs" data-href="<?php p($preview); ?>"><?php p($m['data']->msgto) ?></td>
@@ -308,7 +318,7 @@ $has_multiple_sources = count($sources) > 1;
 						</td>
 						<?php if ($source != 'history') { ?>
 						<td class="hidden-xs hidden-sm">
-							<a title="Release/retry" data-action="retry"><i class="glyphicon glyphicon-play"></i></a>
+							<a title="Release/retry" data-action="retry"><i class="glyphicon glyphicon-play-circle"></i></a>
 						</td>
 						<?php } ?>
 						<td data-href="<?php p($preview); ?>">&nbsp;</td>

static/css/styles.css
@@ -161,7 +161,7 @@ body > .navbar-toolbar {
 }
 .msg-body {
 	max-width: 100%;
-	overflow: scroll;
+	overflow: auto;
 	background-color: #fff;
 }
 

commit 8f71adf94d0dd0ea6a5c17ad115a3cbb1e8d5f40

Fix name on icon, broken in 51c4762e15e39bf54c73bab4e915fc9de34e0819

pages/preview.php
@@ -160,7 +160,7 @@ require_once BASE.'/partials/header.php';
 							<li class="divider"></li>
 							<li><a data-action="delete"><i class="glyphicon glyphicon-trash"></i>&nbsp;Delete message</a></li>
 							<li><a data-action="bounce"><i class="glyphicon glyphicon-repeat"></i>&nbsp;Bounce message</a></li>
-							<li><a data-action="retry"><i class="glyphicon glyphicon-play-cicle"></i>&nbsp;Retry/release message</a></li>
+							<li><a data-action="retry"><i class="glyphicon glyphicon-play-circle"></i>&nbsp;Retry/release message</a></li>
 						<?php } ?>
 					</ul>
 				</li>