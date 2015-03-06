b09ab7c01a23c3391d2a660b76a5a73b1f585e60
Erik Lax <erik@halon.se>
2015-03-06 21:36:03 +0100

    Add stats for cPanel users

--- cpanel-sp-enduser.live.php
+++ cpanel-sp-enduser.live.php
@@ -99,6 +99,9 @@ switch (@$_GET['page'])
 	case 'user':
 		require_once BASE.'/pages/user.php';
 	break;
+	case 'stats':
+		require_once BASE.'/pages/stats.php';
+	break;
 	case 'log':
 		require_once BASE.'/pages/log.php';
 	break;
