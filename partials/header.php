1cc1a2a358992e4a5d1e8e1b8b70b8635ad188f1
Erik Lax <erik@halon.se>
2015-09-25 15:35:00 +0200

    unused code

--- partials/header.php
+++ partials/header.php
@@ -2,20 +2,9 @@
 
 require_once BASE.'/inc/core.php';
 
-$pagename = $settings->getPageName();
-$title = $title ?: 'Untitled';
-$logo = file_exists('template/logo.png') ? 'template/logo.png' : 'static/img/logo.png';
-$styles = file_exists('template/styles.css') ? 'template/styles.css' : 'static/css/styles.css';
-
-function header_active($page)
-{
-	if ($_GET['page'] == $page || ($_GET['page'] == '' && $page == 'index'))
-		echo ' active';
-}
-
 require_once BASE.'/inc/smarty.php';
 
 $smarty->assign('page_active', $_GET['page'] ? $_GET['page'] : 'index');
-$smarty->assign('title', $title);
+$smarty->assign('title', $title ?: 'Untitled');
 
 $smarty->display('header.tpl');
