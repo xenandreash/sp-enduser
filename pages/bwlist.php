d7e588411c5e2870e925d90f7341a2e07e0e7323
Erik Lax <erik@halon.se>
2015-09-28 14:18:41 +0200

    converted black and whitelist page

--- pages/bwlist.php
+++ pages/bwlist.php
@@ -51,40 +51,6 @@ if ($_GET['list'] == 'add') {
 	die();
 }
 
-$title = 'Black/whitelist';
-$javascript[] = 'static/js/bwlist.js';
-require_once BASE.'/partials/header.php';
-
-$row_classes = array(
-	'whitelist' => 'success',
-	'blacklist' => 'danger',
-);
-
-function print_row($type, $value, $accesses, $icon = '')
-{
-	$access = implode(', ', array_map(function($v) { return $v === '' ? '<span class="text-muted">everyone</span>' : htmlspecialchars($v);}, $accesses));
-	if (count($accesses) > 1) $access = '<span class="badge">'.count($accesses).'</span> '.$access;
-?>
-							<td class="hidden-xs" style="width:30px"><?php echo $icon ?></td>
-							<td class="hidden-xs"><?php p($type); ?></td>
-							<td class="hidden-xs"><?php p($value); ?></td>
-							<td class="hidden-xs"><?php echo $access; ?></td>
-							<td class="visible-xs">
-								<p>
-									<span class="glyphicon glyphicon-pencil"></span>&nbsp;
-									<?php p($value); ?>
-								</p>
-								<p>
-									<span class="glyphicon glyphicon-inbox"></span>&nbsp;
-									<?php echo $access; ?>
-								</p>
-							</td>
-							<td style="width: 30px; vertical-align: middle">
-								<a onclick="return confirm('Really delete <?php p($type) ?> <?php p($value) ?> for <?php echo count($accesses)?> recipients?')" title="Remove" href="?page=bwlist&list=delete&access=<?php echo urlencode(implode(',', $accesses)) ?>&type=<?php p($type) ?>&value=<?php echo urlencode($value) ?>"><i class="glyphicon glyphicon-remove"></i></a>
-							</td>
-<?php
-}
-
 $result = array();
 $offset = isset($_GET['offset']) ? intval($_GET['offset']) : 0;
 $limit = isset($_GET['limit']) ? intval($_GET['limit']) : 500;
@@ -171,186 +137,23 @@ $result2 = array();
 foreach ($result as $row)
 	$result2[$row['type']][$row['value']][] = $row['access'];
 
-if ($_GET['error'] == 'perm') {
-?>
-	<div class="container-fluid">
-		<div class="alert alert-danger" role="alert">
-			You are not allowed to add a black/whitelist entry for that recipient.
-		</div>
-	</div>
-<?php
-}
+if ($_GET['error'] == 'perm')
+	$error = 'You are not allowed to add a black/whitelist entry for that recipient.';
 
-?>
-	<div class="container-fluid">
-		<div class="col-md-6 col-lg-8">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h3 class="panel-title">
-						Black/whitelist
-						<a class="pull-right" data-toggle="collapse" href="#search">
-							<span class="glyphicon glyphicon-search"></span>
-						</a>
-					</h3>
-				</div>
-				<div id="search" class="<?php if (!$search) echo 'collapse'; ?>"><div class="panel-body">
-					<form class="form-horizontal" method="get">
-						<input type="hidden" name="page" value="bwlist">
-						<div class="input-group">
-							<span class="input-group-addon"><span class="glyphicon glyphicon-search"></span></span>
-							<input type="text" class="form-control" placeholder="Search for..." name="search" value="<?php p($search) ?>">
-							<span class="input-group-btn">
-								<button class="btn btn-default" type="search">Search</button>
-							</span>
-						</div>
-					</form>
-				</div></div>
-				<table class="table">
-					<thead class="hidden-xs">
-						<tr>
-							<th class="hidden-xs" style="width: 30px"></th>
-							<th class="hidden-xs" style="width: 100px">Type</th>
-							<th class="hidden-xs">Sender</th>
-							<th class="hidden-xs">For recipient</th>
-							<th class="visible-xs"></th>
-							<th style="width: 30px"></th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php
-						$id = 0;
-						foreach ($result2 as $type => $items) {
-							foreach ($items as $value => $accesses) {
-								if (count($accesses) > 1) {
-									$id++;
-						?>
-						<tr style="cursor:pointer" data-toggle="<?php p($id) ?>" class="toggle <?php p($row_classes[$type] ?: 'info'); ?>">
-						<?php print_row($type, $value, $accesses, '<span class="expand-icon glyphicon glyphicon-expand"></span>') ?>
-						</tr>
-						<?php
-									foreach ($accesses as $access) {
-						?>
-						<tr style="display:none" class="hidden-<?php p($id) ?> <?php p($row_classes[$type] ?: 'info'); ?>">
-						<?php print_row($type, $value, array($access), '<sup style="opacity:.5">L</sup>') ?>
-						</tr>
-						<?php
-									}
-								} else {
-						?>
-						<tr class="<?php p($row_classes[$type] ?: 'info'); ?>">
-						<?php print_row($type, $value, $accesses) ?>
-						</tr>
-						<?php
-								}
-							}
-						}
-						if (count($result) == 0)
-							echo '<tr><td colspan="4" class="text-muted">No black/whitelist</td></tr>';
-						?>
-					</tbody>
-				</table>
-			</div>
-			<nav>
-				<?php if ($total) { // MySQL SQL_CALC_FOUND_ROWS version?>
-				<ul class="pagination">
-					<?php foreach ($pages as $p) { ?>
-						<?php if ($p === '...') { ?>
-						<li class="disabled"><a href="#">...</a></li>
-						<?php } else if ($p == $currpage) { ?>
-						<li class="active"><a href="#"><?php p($p+1)?></a></li>
-						<?php } else { ?>
-						<li><a href="?page=bwlist&offset=<?php p($limit*$p) ?>&limit=<?php p($limit); ?>&search=<?php p($search); ?>"><?php p($p+1)?></a></li>
-						<?php } ?>
-					<?php } ?>
-				</ul>
-				<?php } else { ?>
-				<ul class="pager">
-					<li class="previous<?php if ($offset == 0) p(" disabled") ?>"><a href="javascript:history.go(-1);"><span aria-hidden="true">&larr;</span> Previous</a></li>
-					<li class="next<?php if (!$pagemore) p(" disabled") ?>"><a href="?page=bwlist&offset=<?php p($offset + $limit); ?>&limit=<?php p($limit); ?>&search=<?php p($search); ?>">Next <span aria-hidden="true">&rarr;</span></a></li>
-				</ul>
-				<?php } ?>
-			</nav>
-		</div>
-		<div class="col-md-6 col-lg-4">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h3 class="panel-title">Add...</h3>
-				</div>
-				<div class="panel-body">
-					<form class="form-horizontal" action="?page=bwlist&list=add" method="post">
-						<div class="form-group">
-							<label for="type" class="control-label col-md-3">Action</label>
-							<div class="col-md-9">
-								<select name="type" class="form-control">
-									<option value="blacklist">Blacklist</option>
-									<option value="whitelist">Whitelist</option>
-								</select>
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-md-3">Sender</label>
-							<div class="col-md-9">
-								<input type="text" class="form-control" name="value">
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-md-3" style="white-space: nowrap;">For recipient</label>
-							<div class="col-md-9">
-								<?php
-									$_access = array();
-									foreach (Session::Get()->getAccess() as $type => $accesses)
-										if ($type == 'mail' || $type == 'domain')
-											foreach ($accesses as $a)
-												$_access[] = $a;
-									if (count($_access) == 1) {
-								?>
-									<input type="hidden" class="form-control" name="access[]" value="<?php p($_access[0]); ?>">
-									<p class="form-control-static"><?php p($_access[0]); ?></p>
-								<?php
-									} else if (count($_access) > 0) {
-								?>
-									<button id="check-all" class="btn btn-info">Select all</button>
-									<button id="add-access" class="btn btn-default">Add custom</button>
-									<?php if (count($_access) > 5) { ?>
-									<div class="panel panel-default" style="height: 115px; padding-left: 10px; margin-top: 5px; overflow-y: scroll;">
-									<?php } ?>
-									<div id="extra-accesses"></div>
-									<?php foreach ($_access as $a) { ?>
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" class="recipient" name="access[]" value="<?php p($a); ?>">
-											<?php p($a); ?>
-										</label>
-									</div>
-									<?php } ?>
-									<?php if (count($_access) > 5) { ?>
-									</div>
-									<?php } ?>
-								</select>
-								<?php } else { ?>
-									<input type="text" class="form-control" name="access[]" placeholder="everyone">
-								<?php } ?>
-								<p class="help-block">
-									Sender may be an IP address, an e-mail address, a domain name or a wildcard domain name starting with a dot (eg. .co.uk).
-								</p>
-							</div>
-						</div>
-						<div class="col-md-offset-3 col-md-9">
-							<button type="submit" class="btn btn-primary">Add</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-	<style>
-		table {
-			table-layout: fixed;
-		}
-		td {
-			text-overflow: ellipsis;
-			white-space: nowrap;
-			overflow: hidden;
-		}
-	</style>
-<?php require_once BASE.'/partials/footer.php'; ?>
+$javascript[] = 'static/js/bwlist.js';
+
+require_once BASE.'/inc/smarty.php';
+
+if ($error) $smarty->assign('error', $error);
+if ($search) $smarty->assign('search', $search);
+$access = Session::Get()->getAccess();
+$smarty->assign('useraccess', empty($access)?array():@array_merge($access['mail'], $access['domain']));
+$smarty->assign('items', $result2);
+if ($total) $smarty->assign('total', $total);
+if ($pages) $smarty->assign('pages', $pages);
+$smarty->assign('currpage', $currpage);
+$smarty->assign('limit', $limit);
+$smarty->assign('offset', $offset);
+if ($pagemore) $smarty->assign('pagemore', true);
+
+$smarty->display('bwlist.tpl');
---
+++ templates/bwlist.tpl
@@ -0,0 +1,203 @@
+{include file='header.tpl' title='Black/whitelist' page_active='bwlist'}
+{if $error}
+<div class="container-fluid">
+	<div class="alert alert-danger" role="alert">
+		{$error}
+	</div>
+</div>
+{/if}
+<div class="container-fluid">
+	<div class="col-md-6 col-lg-8">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">
+					Black/whitelist
+					<a class="pull-right" data-toggle="collapse" href="#search">
+						<span class="glyphicon glyphicon-search"></span>
+					</a>
+				</h3>
+			</div>
+			<div id="search" class="{if not $search}collapse{/if}"><div class="panel-body">
+				<form class="form-horizontal" method="get">
+					<input type="hidden" name="page" value="bwlist">
+					<div class="input-group">
+						<span class="input-group-addon"><span class="glyphicon glyphicon-search"></span></span>
+						<input type="text" class="form-control" placeholder="Search for..." name="search" value="{$search|escape}">
+						<span class="input-group-btn">
+							<button class="btn btn-default" type="search">Search</button>
+						</span>
+					</div>
+				</form>
+			</div></div>
+			<table class="table">
+				<thead class="hidden-xs">
+					<tr>
+						<th class="hidden-xs" style="width: 30px"></th>
+						<th class="hidden-xs" style="width: 100px">Type</th>
+						<th class="hidden-xs">Sender</th>
+						<th class="hidden-xs">For recipient</th>
+						<th class="visible-xs"></th>
+						<th style="width: 30px"></th>
+					</tr>
+				</thead>
+				<tbody>
+				{$id = 0}
+				{foreach from=$items key=type item=item}
+					{foreach from=$item key=value item=accesses}
+						{if count($accesses) > 1}
+							{$id = $id + 1}
+							<tr style="cursor:pointer" data-toggle="{$id}" class="toggle {if $type=='whitelist'}success{elseif $type=='blacklist'}danger{else}info{/if}">
+								<td class="hidden-xs" style="width:30px"><span class="expand-icon glyphicon glyphicon-expand"></span></td>
+								<td class="hidden-xs">{$type}</td>
+								<td class="hidden-xs">{$value|escape}</td>
+								<td class="hidden-xs"><span class="badge">{count($accesses)}</span></td>
+								<td class="visible-xs">
+									<p>
+										<span class="glyphicon glyphicon-pencil"></span>&nbsp; {$value|escape}
+									</p>
+									<p>
+										<span class="glyphicon glyphicon-inbox"></span>&nbsp;
+										<span class="badge">{count($accesses)}</span>
+									</p>
+								</td>
+								<td style="width: 30px; vertical-align: middle">
+									<a onclick="return confirm('Really delete {$type} {$value|addslashes} for {count($accesses)} recipients?')" title="Remove" href="?page=bwlist&list=delete&access={implode(',', $accesses)|urlencode}&type={$type}&value={$value|urlencode}"><i class="glyphicon glyphicon-remove"></i></a>
+								</td>
+							</tr>
+							{foreach $accesses as $access}
+							<tr style="display:none" class="hidden-{$id} {if $type=='whitelist'}success{elseif $type=='blacklist'}danger{else}info{/if}">
+								<td class="hidden-xs" style="width:30px"><sup style="opacity:.5">L</sup></td>
+								<td class="hidden-xs">{$type}</td>
+								<td class="hidden-xs">{$value|escape}</td>
+								<td class="hidden-xs">{if $access}{$access|escape}{else}<span class="text-muted">everyone</span>{/if}</td>
+								<td class="visible-xs">
+									<p>
+										<span class="glyphicon glyphicon-pencil"></span>&nbsp; {$value|escape}
+									</p>
+									<p>
+										<span class="glyphicon glyphicon-inbox"></span>&nbsp;
+										{if $access}{$access|escape}{else}<span class="text-muted">everyone</span>{/if}
+									</p>
+								</td>
+								<td style="width: 30px; vertical-align: middle">
+									<a onclick="return confirm('Really delete {$type} {$value|addslashes} for 1 recipient?')" title="Remove" href="?page=bwlist&list=delete&access={$access|urlencode}&type={$type}&value={$value|urlencode}"><i class="glyphicon glyphicon-remove"></i></a>
+								</td>
+							</tr>
+							{/foreach}
+						{else}
+							<tr class="{if $type=='whitelist'}success{elseif $type=='blacklist'}danger{else}info{/if}">
+								<td class="hidden-xs" style="width:30px"></td>
+								<td class="hidden-xs">{$type}</td>
+								<td class="hidden-xs">{$value|escape}</td>
+								<td class="hidden-xs">{if $accesses.0}{$accesses.0|escape}{else}<span class="text-muted">everyone</span>{/if}</td>
+								<td class="visible-xs">
+									<p>
+										<span class="glyphicon glyphicon-pencil"></span>&nbsp; {$value|escape}
+									</p>
+									<p>
+										<span class="glyphicon glyphicon-inbox"></span>&nbsp;
+										{if $accesses.0}{$accesses.0|escape}{else}<span class="text-muted">everyone</span>{/if}
+									</p>
+								</td>
+								<td style="width: 30px; vertical-align: middle">
+									<a onclick="return confirm('Really delete {$type} {$value|addslashes} for {count($accesses)} recipients?')" title="Remove" href="?page=bwlist&list=delete&access={$accesses.0|urlencode}&type={$type}&value={$value|urlencode}"><i class="glyphicon glyphicon-remove"></i></a>
+								</td>
+							</tr>
+						{/if}
+					{/foreach}
+				{foreachelse}
+					<tr><td colspan="4" class="text-muted">No black/whitelist</td></tr>
+				{/foreach}
+				</tbody>
+			</table>
+		</div>
+		<nav>
+			{if $total}
+			<ul class="pagination">
+				{foreach $pages as $p}
+					{if $p === '...'}
+					<li class="disabled"><a href="#">...</a></li>
+					{elseif $p === $currpage}
+					<li class="active"><a href="#">{$p+1}</a></li>
+					{else}
+					<li><a href="?page=bwlist&offset={$limit*$p}&limit={$limit}&search={$search|urlencode}">{$p+1}</a></li>
+					{/if}
+				{/foreach}
+			</ul>
+			{else}
+			<ul class="pager">
+				<li class="previous{if $offset == 0} disabled{/if}"><a href="javascript:history.go(-1);"><span aria-hidden="true">&larr;</span> Previous</a></li>
+				<li class="next{if !$pagemore} disabled{/if}"><a href="?page=bwlist&offset={$offset+$limit}&limit={$limit}&search={$search|urlencode}">Next <span aria-hidden="true">&rarr;</span></a></li>
+			</ul>
+			{/if}
+		</nav>
+	</div>
+	<div class="col-md-6 col-lg-4">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Add...</h3>
+			</div>
+			<div class="panel-body">
+				<form class="form-horizontal" action="?page=bwlist&list=add" method="post">
+					<div class="form-group">
+						<label for="type" class="control-label col-md-3">Action</label>
+						<div class="col-md-9">
+							<select name="type" class="form-control">
+								<option value="blacklist">Blacklist</option>
+								<option value="whitelist">Whitelist</option>
+							</select>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="control-label col-md-3">Sender</label>
+						<div class="col-md-9">
+							<input type="text" class="form-control" name="value">
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="control-label col-md-3" style="white-space: nowrap;">For recipient</label>
+						<div class="col-md-9">
+							{if count($useraccess) == 1}
+								<input type="hidden" class="form-control" name="access[]" value="{$useraccess.0|escape}">
+								<p class="form-control-static">{$useraccess.0|escape}</p>
+							{elseif count($useraccess) > 0}
+								<button id="check-all" class="btn btn-info">Select all</button>
+								<button id="add-access" class="btn btn-default">Add custom</button>
+								{if count($useraccess) > 5}<div class="panel panel-default" style="height: 115px; padding-left: 10px; margin-top: 5px; overflow-y: scroll;">{/if}
+								<div id="extra-accesses"></div>
+								{foreach $useraccess as $a}
+								<div class="checkbox">
+									<label>
+										<input type="checkbox" class="recipient" name="access[]" value="{$a|escape}"> {$a|escape}
+									</label>
+								</div>
+								{/foreach}
+								{if count($useraccess) > 5}</div>{/if}
+							</select>
+							{else}
+								<input type="text" class="form-control" name="access[]" placeholder="everyone">
+							{/if}
+							<p class="help-block">
+								Sender may be an IP address, an e-mail address, a domain name or a wildcard domain name starting with a dot (eg. .co.uk).
+							</p>
+						</div>
+					</div>
+					<div class="col-md-offset-3 col-md-9">
+						<button type="submit" class="btn btn-primary">Add</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+<style>
+	table {
+		table-layout: fixed;
+	}
+	td {
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		overflow: hidden;
+	}
+</style>
+{include file='footer.tpl'}
