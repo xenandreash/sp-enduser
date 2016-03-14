de88ddb3b6a1474a623cfcce82dd047c88e53127
Erik Lax <erik@halon.se>
2016-03-14 15:59:29 -0400

    Made the code fail-safe

--- modules/Login.database.php
+++ modules/Login.database.php
@@ -8,18 +8,24 @@ function halon_login_database($username, $password, $method, $settings)
 {
 	$dbh = $settings->getDatabase();
 	$statement = $dbh->prepare("SELECT * FROM users WHERE username = :username;");
-	$statement->execute(array(':username' => $username));
+	if (!$statement->execute(array(':username' => $username)))
+		return false;
+
 	$row = $statement->fetch(PDO::FETCH_ASSOC);
 	if (!$row || !password_verify($password, $row['password']))
+		return false;
+
+	$statement = $dbh->prepare("SELECT * FROM users_relations WHERE username = :username;");
+	if (!$statement->execute(array(':username' => $row['username'])))
 		return false;
 
 	$result = array();
 	$result['username'] = $row['username'];
 	$result['source'] = 'database';
 	$result['access'] = array();
-	$statement = $dbh->prepare("SELECT * FROM users_relations WHERE username = :username;");
-	$statement->execute(array(':username' => $row['username']));
+
 	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 		$result['access'][$row['type']][] = $row['access'];
+
 	return $result;
 }
