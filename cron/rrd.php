c554a291076973ed74ac5553ea06be2aefa7632c
Erik Lax <erik@halon.se>
2016-04-14 10:11:24 +0200

    lock if executions are overlapping

--- cron/rrd.php
+++ cron/rrd.php
@@ -7,6 +7,14 @@ require_once BASE.'/inc/core.php';
 
 $dbh = $settings->getDatabase();
 
+$fp = fopen($settings->getGraphPath().'/rrd.lock', 'w+');
+if (!$fp || !flock($fp, LOCK_EX | LOCK_NB))
+{
+	syslog(LOG_WARNING, 'RRD is running to slow (multiple executions are overlapping by cron)');
+	fclose($fp);
+	exit(1);
+}
+
 $q = $dbh->query('SELECT SUM(reject) AS reject, SUM(deliver) AS deliver, domain FROM stat GROUP BY domain;');
 while ($row = $q->fetch(PDO::FETCH_ASSOC))
 {
@@ -21,6 +29,9 @@ while ($row = $q->fetch(PDO::FETCH_ASSOC))
 		echo rrd_error()."\n";
 	}
 }
+
+flock($fp, LOCK_UN);
+fclose($fp);
 
 function halon_rrd_create($name, $legends)
 {
