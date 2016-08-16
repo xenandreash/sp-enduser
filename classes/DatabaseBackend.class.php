96ae53e10d0576cb8e2bc4ff57e7d0c0b3733ba4
Anders Berggren <anders@desh.se>
2016-08-16 14:31:31 +0200

    Make search results distinct

--- classes/DatabaseBackend.class.php
+++ classes/DatabaseBackend.class.php
@@ -24,14 +24,18 @@ class DatabaseBackend extends Backend
 		$sql_select = 'UNIX_TIMESTAMP(msgts0) AS msgts0 FROM '.Session::Get()->getMessagelogTable();
 		$sql_where = hql_to_sql($search);
 		$real_sql = $this->restrict_select($sql_select, $sql_where, 'ORDER BY id DESC', intval($size + 1), $param);
-		$real_sql['sql'] .= ' ORDER BY id DESC LIMIT '.intval($size + 1); // don't send unnecessary
 		
 		// Fetch stuff
 		try {
 			$statement = $this->database->prepare($real_sql['sql']);
 			$statement->execute($real_sql['params']);
-			while ($item = $statement->fetchObject())
+			$dup = [];
+			while ($item = $statement->fetchObject()) {
+				if (isset($dup[$item->id]))
+					continue;
+				$dup[$item->id] = true;
 				$results[$item->msgts0][] = array('id' => $item->union_id, 'type' => 'log', 'data' => $item);
+			}
 		} catch(Exception $e) {
 			$errors[] = $e->getMessage();
 		}
