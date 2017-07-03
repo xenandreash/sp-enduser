4146e24b6d1e0de3d52e44ed68b9d862ca35cf7b
Johan Harén <johan@halon.io>
2017-07-03 11:59:10 +0200

    Fix indent

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!isset($_SERVER['argc']))
-    die('This file can only be run from command line');
+	die('This file can only be run from command line');
 
 define('UPDATE_IGUARD', true);
 define('BASE', dirname(__FILE__));
@@ -14,29 +14,29 @@ $dbh = $settings->getDatabase();
 $updateSkip = null;
 
 switch (@$argv[1]) {
-    case '--skip': 
-        $updateSkip = $argv[2];
-        break;
-    case 'check':
-        exit($version['update_required'] ? 0 : 1);
+	case '--skip':
+		$updateSkip = $argv[2];
+		break;
+	case 'check':
+		exit($version['update_required'] ? 0 : 1);
 }
 
 if ($version['update_required']) {
-    $a = getAvailableVersion();
-    for ($v = getCurrentVersion()+1; $v <= $a; $v++) {
-        if ($updateSkip != $v) {
-            echo "Running [$v.php] - Updating from version ".getCurrentVersion()." to $a\n";
-            $errCode = (require_once BASE.'/updates/'.$v.'.php');
-            if ($errCode)
-                die("Error - Unable to apply update (Return code: $errCode)\n");
-        } else {
-            echo "Skipping [$v.php]... \n";
-        }
-        $dbh->exec('UPDATE dbversion SET current = '.$v.';');
-        if ($v != getCurrentVersion())
-            die("Error - Unable to update database to version $v\n");
-    }
-    echo "Update completed.\n";
+	$a = getAvailableVersion();
+	for ($v = getCurrentVersion()+1; $v <= $a; $v++) {
+		if ($updateSkip != $v) {
+			echo "Running [$v.php] - Updating from version ".getCurrentVersion()." to $a\n";
+			$errCode = (require_once BASE.'/updates/'.$v.'.php');
+			if ($errCode)
+				die("Error - Unable to apply update (Return code: $errCode)\n");
+		} else {
+			echo "Skipping [$v.php]... \n";
+		}
+		$dbh->exec('UPDATE dbversion SET current = '.$v.';');
+		if ($v != getCurrentVersion())
+			die("Error - Unable to update database to version $v\n");
+	}
+	echo "Update completed.\n";
 } else {
-    die("Already up-to-date.\n");
+	die("Already up-to-date.\n");
 }
--- updates/1.php
+++ updates/1.php
@@ -4,16 +4,16 @@ if (!defined('UPDATE_IGUARD')) die('File not included');
 
 $statement = $dbh->prepare('SELECT * FROM dbversion LIMIT 1;');
 if (!$statement || $statement->execute() === false) {
-    echo "Adding table dbversion... ";
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $dbh->exec('CREATE TABLE dbversion (current INTEGER);');
-    echo "Done\n";
+	echo "Adding table dbversion... ";
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$dbh->exec('CREATE TABLE dbversion (current INTEGER);');
+	echo "Done\n";
 
-    echo "Inserting values to dbversion... ";
-    $dbh->exec('INSERT INTO dbversion (current) VALUES (0);');
-    echo "Done\n";
-    
-    return 0;
+	echo "Inserting values to dbversion... ";
+	$dbh->exec('INSERT INTO dbversion (current) VALUES (0);');
+	echo "Done\n";
+
+	return 0;
 } else {
-    return 1;
+	return 1;
 }
