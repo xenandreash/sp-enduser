339a46e90034ee7413cf7bc9d2c553f3df983bf3
Johan Harén <johan@halon.io>
2018-01-04 15:11:19 +0100

    Fix indent in version.php

--- inc/version.php
+++ inc/version.php
@@ -3,25 +3,25 @@
 $version['update_required'] = false;
 
 function getCurrentVersion() {
-    global $settings;
-    $dbh = $settings->getDatabase();
-    $statement = $dbh->prepare('SELECT current FROM dbversion');
-    if ($statement && $statement->execute()) {
-        $result = $statement->fetch(PDO::FETCH_ASSOC);
-        return $result['current'];
-    } else {
-        return 0;
-    }
+	global $settings;
+	$dbh = $settings->getDatabase();
+	$statement = $dbh->prepare('SELECT current FROM dbversion');
+	if ($statement && $statement->execute()) {
+		$result = $statement->fetch(PDO::FETCH_ASSOC);
+		return $result['current'];
+	} else {
+		return 0;
+	}
 }
 
 function getAvailableVersion() {
-    return (int) file_get_contents(BASE.'/updates/version.txt');
+	return (int) file_get_contents(BASE.'/updates/version.txt');
 }
 
 function isUpdateRequired() {
-    return getAvailableVersion() > getCurrentVersion();
+	return getAvailableVersion() > getCurrentVersion();
 }
 
 if ($settings->getDatabase()) {
-    $version['update_required'] = isUpdateRequired();
+	$version['update_required'] = isUpdateRequired();
 }
